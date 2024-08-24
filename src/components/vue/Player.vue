de70325d9630b196121ca022f693782163e2bfdd
Skid Vis <skid@skidvis.com>
2024-08-24 16:29:24 -0500

    skat 1 complete

---
+++ src/components/vue/Player.vue
@@ -0,0 +1,72 @@
+<template>
+    <div v-if="currentPage != null" class="flex justify-center items-center flex-col md:flex-row">
+        <div class="min-w-min">
+            <img :src="'/'+ meta.root + '/' + currentPage.file" alt="" class="m-auto">
+        </div>
+        <div class="p-1 max-w-md">
+            <ul>
+                <li v-for="caption in currentPage.captions" class="p-1 m-2 border border-yellow-50 rounded-lg bg-yellow-100 font-medium">{{ caption }}</li>
+            </ul>
+        </div>
+    </div>
+    <ul class="flex justify-center items-center p-3 gap-3">
+        <li v-for="choice in currentPage.choices" class="flex">
+            <button @click="nextPage(choice.url)" class="border p-1 border-solid m-auto bg-lime-700 hover:bg-lime-800 font-bold text-white rounded-sm">{{ choice.text }}</button>
+        </li>
+    </ul>
+    <div class="flex justify-evenly items-center p-3 gap-3">
+        <div class="font-bold text-center">Good Endings Discovered: {{ goodEndingsFound }}/{{meta.goodEndings}}</div>
+        <div class="font-bold text-center">Bad Endings Discovered: {{ badEndingsFound }}/{{meta.badEndings}}</div>
+    </div>
+</template>
+
+<script setup>
+    import {meta, data} from '../../data/skat1/info.js';
+    import { ref } from 'vue';
+
+    const currentPage = ref(null);
+    const blank = {
+        file: meta.cover,
+        captions: meta.description,
+        choices: [
+            {
+                text: 'Begin',
+                url: 'a0'
+            }
+        ]
+    }    
+    let audioElement = null;
+    let goodEndingsFound = 0;
+    let badEndingsFound = 0;
+    //countBadEndings();
+
+    if(data) currentPage.value = blank;
+
+    function nextPage(url){        
+        if(url === 'winner') goodEndingsFound++;
+        if(url === 'bad') badEndingsFound++;
+        if(url === 'bad' || url === 'winner') url = 'a0';
+        const result = data.find(item => item.id === url);
+        currentPage.value = result;
+        playAudio(result.id);
+    }
+
+    function playAudio(clip) {
+        if(audioElement != null) audioElement.pause();
+        audioElement = new Audio('/'+ meta.root + '/audio/' + clip + '.mp3');
+        audioElement.load();   
+        audioElement.play();    
+    }
+
+    function countBadEndings(){
+        data.forEach(element => {
+            element.choices.forEach(choice => {
+                if(choice.url === 'bad') meta.badEndings++;
+                if(choice.url === 'winner') meta.goodEndings++;
+            })
+        });
+    }
+</script>
+
+<style>    
+</style>
---
+++ src/data/skat1/info.js
@@ -0,0 +1,541 @@
+const meta = {
+  "root": "skat1",
+  "cover": "a0.gif",
+  "badEndings": 7,
+  "goodEndings": 1,
+  "description": [
+    "Step into the shadows with Skat, a skilled warrior of the enigmatic ShadowBlades - a clan of ninja cats shrouded in secrecy. When Skat's childhood friend Strix, a bunny raised among the felines, hands him a mysterious scroll, it sets in motion a thrilling adventure that will test his loyalty, skills, and very survival. From the corridors of power to the savage lands of the wolf tribe, your choices will guide Skat through a maze of political intrigue, fierce battles, and the hunt for the elusive Gemini Crystal.",    
+    "But in this world of ninja cats and cunning foes, nothing is as it seems. Will you choose stealth or strength? Loyalty or survival? Each decision could spell the difference between glory and doom. As you navigate this pulse-pounding journey, you'll find yourself entangled in a web of betrayal, friendship, and deadly consequences. The fate of Skat, the ShadowBlades, and perhaps the entire world rests in your hands. Are you ready to step into the shadows and uncover the twisting paths that await?"
+  ]
+}
+
+const data = [
+  {
+    "id": "a0",
+    "file": "a0.gif",
+    "captions": ["Skat is a member of the Ninja Cat Organization known as The ShadowBlades. He is one of their most powerful warriors, matched only by his best friend, Strix. Strix is a bunny that was raised by the ShadowBlades. In this issue, we will see how strong their friendship is... or will we? You decide."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "a1"
+      }
+    ]
+  },
+  {
+    "id": "a1",
+    "file": "a1.gif",
+    "captions": ["Upon returning to his home, Skat discovers one of his fellow clan members, Strix. Strix gives Skat a scroll with specific instructions for a mission. Along with the scroll, Skat is given one-half of the payment for the mission's expenses. \"Do you accept the mission, or refuse?\" Strix asks."],
+    "choices": [
+      {
+        "text": "Accept the mission?",
+        "url": "b1"
+      },
+      {
+        "text": "Refuse the mission?",
+        "url": "c1"
+      }
+    ]
+  },
+  {
+    "id": "b1",
+    "file": "b1.gif",
+    "captions": ["The scroll reads: \"Skat, you have been chosen by the ShadowBlades to go on an important task. We need you to convince the Mayor of the Dogs to build us a special battleship. Cost means nothing to us; give him whatever he asks of you. The reasons for our request are best left unknown to you, but great significance rests on your success. Good luck.\""],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "b2"
+      }
+    ]
+  },
+  {
+    "id": "b2",
+    "file": "b2.gif",
+    "captions": ["Skat wastes no time and heads straight to the Mayor's building. After hearing the request for the special battleship, Mayor Bawer demands payment from Skat in the form of a rare stone called the Gemini Crystal."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "b3"
+      }
+    ]
+  },
+  {
+    "id": "b3",
+    "file": "b3.gif",
+    "captions": ["If Mayor Bawer is to build the special battleship, Skat must first retrieve the Gemini Crystal from the wolf tribe. Skat agrees to the Mayor's terms and sets out for the Crystal. But how will he get such a well-guarded item? Skat must once again make an important decision on his mission."],
+    "choices": [
+      {
+        "text": "Use force to take the Gemini Crystal!",
+        "url": "d1"
+      },
+      {
+        "text": "Use stealth to steal the Gemini Crystal!",
+        "url": "e1"
+      }
+    ]
+  },
+  {
+    "id": "c1",
+    "file": "c1.gif",
+    "captions": ["'Refuse!?' Strix screams. 'No one insults the clan with such betrayal. You surprise me, Skat. Though we are brothers, I mustn't let you, or anyone, bring shame to our clan. Now prepare for punishment!' Skat knows Strix, and this threat is no bluff. He will attack."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "c2"
+      }
+    ]
+  },
+  {
+    "id": "c2",
+    "file": "c2.gif",
+    "captions": ["Skat has two of his favorite weapons mounted on the wall nearby: his Katana sword and his dragon's star. Certain death will come to Skat if he fights hand-to-hand. Quickly pick one before Strix turns him into a bloody pulp. Hurry!"],
+    "choices": [
+      {
+        "text": "Choose the Katana!",
+        "url": "f1"
+      },
+      {
+        "text": "Choose the Dragon Star",
+        "url": "g1"
+      }
+    ]
+  },
+  {
+    "id": "d1",
+    "file": "d1.gif",
+    "captions": ["After a long trip, Skat reaches the Wolves' village and proceeds to invade with brutal force. The further he progresses into the village, the stronger the wolf soldiers attack. Amazingly, Skat beats them all."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "d2"
+      }
+    ]
+  },
+  {
+    "id": "d2",
+    "file": "d2.gif",
+    "captions": ["Finally, he succeeds, gaining access to the room where the Gemini Crystal is being held. A strange yellow glow illuminates the stone, and Skat gazes for a moment before moving towards it."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "d3"
+      }
+    ]
+  },
+  {
+    "id": "d3",
+    "file": "d3.gif",
+    "captions": ["As he reaches for the Crystal, a thunderous roar fills the room. 'Intruder!' Skat turns around and sees he is surrounded by wolves. One of them steps forward. 'You are a skilled fighter and have gotten surprisingly far,' the wolf chief named Kendrick warns Skat. 'Leave now, and we won't kill you. Stay... and you will die!'"],
+    "choices": [
+      {
+        "text": "Admit defeat and walk away.",
+        "url": "h1"
+      },
+      {
+        "text": "Challenge Kendrick to a fight!",
+        "url": "i1"
+      }
+    ]
+  },
+  {
+    "id": "e1",
+    "file": "e1.gif",
+    "captions": ["Skat decides to sneak into the Wolf village by scaling the wall and striking the patrol towers with the Dragon's star. He succeeds and proceeds to enter a building where he believes they would most likely keep the Gemini Crystal."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "e2"
+      }
+    ]
+  },
+  {
+    "id": "e2",
+    "file": "e2.gif",
+    "captions": ["His suspicion has served him well. Skat has found the Crystal. 'This was too easy,' Skat thinks to himself as he grabs it and puts it in his pouch. At that moment, a loud siren begins to sound. 'I've been discovered! Time to get out of here, but which way? I obviously can't use the front door. I'll be outnumbered and killed.'"],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "e3"
+      }
+    ]
+  },
+  {
+    "id": "e3",
+    "file": "e3.gif",
+    "captions": ["Skat must find a way to escape with the Crystal and his life. He can go out the window, or he can look around and see if there's a secret passage that can lead him to freedom."],
+    "choices": [
+      {
+        "text": "Jump out the window!",
+        "url": "j1"
+      },
+      {
+        "text": "Look for another way out!",
+        "url": "k1"
+      }
+    ]
+  },
+  {
+    "id": "f1",
+    "file": "f1.gif",
+    "captions": ["Strix launches toward Skat at an incredible speed, but Skat's reflexes are enough to clear him from danger and reach for his sword."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "f2"
+      }
+    ]
+  },
+  {
+    "id": "f2",
+    "file": "f2.gif",
+    "captions": ["Strix looks at Skat and smiles. 'I knew you wouldn't be so easy to kill, Skat,' he whispers. Even though Strix has no weapon, Skat knows his chance of survival is 50-50. Both stay motionless, waiting for the other to make the first move. This lasts a great while before Strix moves in with another attack."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "f3"
+      }
+    ]
+  },
+  {
+    "id": "f3",
+    "file": "f3.gif",
+    "captions": ["Skat's patience has paid off, and he slashes Strix in the chest. A power move that would otherwise kill any other opponent drops Strix to the ground, leaving him open for another attack. A deathblow. Skat stands over Strix's wounded body. 'Can I really kill him? We've known each other since childhood. Do I end it... like this?'"],
+    "choices": [
+      {
+        "text": "KILL HIM!",
+        "url": "l1"
+      },
+      {
+        "text": "Let him go.",
+        "url": "m1"
+      }
+    ]
+  },
+  {
+    "id": "g1",
+    "file": "g1.gif",
+    "captions": ["Skat jumps, escaping Strix's attack and grabbing the Dragon's star. 'Hehehe,' Strix laughs out loud. 'You're a fool to think you can win with that silly toy. Come on, give it your best shot!'"],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "g2"
+      }
+    ]
+  },
+  {
+    "id": "g2",
+    "file": "g2.gif",
+    "captions": ["Skat takes aim and shoots the Dragon's star full strength at Strix... He misses. Strix uses the opportunity to his advantage and attacks again—"],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "g3"
+      }
+    ]
+  },
+  {
+    "id": "g3",
+    "file": "g3.gif",
+    "captions": ["He connects the first punch and continues with a flurry of rapid hits to the head, chest, and stomach. Strix shows no mercy and stops only to check for signs of life."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "g4"
+      }
+    ]
+  },
+  {
+    "id": "g4",
+    "file": "g4.gif",
+    "captions": ["Skat is surprisingly still alive. 'You were always a great fighter, Skat,' Strix says. 'Why did you go against the clan?' Strix prepares to deliver the final strike, but he hesitates for just a moment. 'We were friends,' Strix thinks to himself. 'Could this truly be the end of it all?' This could be Skat's last chance to make a comeback. Should he try one last attack, or is he just too weak to win at this point?"],
+    "choices": [
+      {
+        "text": "One more try!",
+        "url": "n1"
+      },
+      {
+        "text": "Leave Skat at the mercy of Strix.",
+        "url": "o1"
+      }
+    ]
+  },
+  {
+    "id": "h1",
+    "file": "h1.gif",
+    "captions": ["Skat decides to surrender. He returns the Gemini Crystal to its original place, and Kendrick makes good on his word by allowing Skat to leave unharmed. It was just too obvious; he was outnumbered 10 to 1. Any smart warrior would have done the same."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "h2"
+      }
+    ]
+  },
+  {
+    "id": "h2",
+    "file": "h2.gif",
+    "captions": ["Other warriors would have let their pride get to them, fighting to the bitter end even knowing there's no way to win, but not Skat. His is an honorable end. He's satisfied with his results, as long as he knows he tried his best..."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "h3"
+      }
+    ]
+  },
+  {
+    "id": "h3",
+    "file": "h3.gif",
+    "captions": ["...Besides, he can always try again later. END."],
+    "choices": [
+      {
+        "text": "The",
+        "url": "bad"
+      }
+    ]
+  },
+  {
+    "id": "i1",
+    "file": "i1.gif",
+    "captions": ["Skat must succeed at any cost. With no fear in his heart, he strikes at the crowd of bloodthirsty wolves."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "i2"
+      }
+    ]
+  },
+  {
+    "id": "i2",
+    "file": "i2.gif",
+    "captions": ["But he is quickly overwhelmed by the pack, and Kendrick snatches the Gemini Crystal. No mercy is shown to intruders, and Skat was no exception. The savage wolves feed on his corpse for hours until only his head remains."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "i3"
+      }
+    ]
+  },
+  {
+    "id": "i3",
+    "file": "i3.gif",
+    "captions": ["As a warning to all others who should dare to intrude on the wolf village with ill will, the Chief, Kendrick, had Skat's head mounted at the entrance. From that night on, no one would dare to trespass onto the village, and the Crystal was safe from all outsiders. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      },
+    ]
+  },
+  {
+    "id": "j1",
+    "file": "j1.gif",
+    "captions": ["Skat leaps out the window and disappears into the night with the Gemini Crystal in his pouch. By the time the wolves notice the stone's been stolen, it's too late."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "j2"
+      }
+    ]
+  },
+  {
+    "id": "j2",
+    "file": "j2.gif",
+    "captions": ["Skat's mission is going as planned. He heads toward his village now. Mayor Bawer is pleased with the Gemini Crystal...."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "j3"
+      }
+    ]
+  },
+  {
+    "id": "j3",
+    "file": "j3.gif",
+    "captions": ["He orders his people to begin construction on the ShadowBlades' special battleship."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "j4"
+      }
+    ]
+  },
+  {
+    "id": "j4",
+    "file": "j4.gif",
+    "captions": ["Skat delivers the news of his successful mission through Strix and is later rewarded with the other half of his payment plus a bonus. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "winner"
+      }
+    ]
+  },
+  {
+    "id": "k1",
+    "file": "k1.gif",
+    "captions": ["Skat searches desperately for the secret passageway, but he can't seem to find it. As he continues to look, he can hear footsteps getting closer."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "k2"
+      }
+    ]
+  },
+  {
+    "id": "k2",
+    "file": "k2.gif",
+    "captions": ["Too late. Skat is discovered, and the entire wolf village moves in and attacks him. It's a slaughter; they gang up on Skat and eat him alive."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "k3"
+      }
+    ]
+  },
+  {
+    "id": "k3",
+    "file": "k3.gif",
+    "captions": ["Mayor Bawer denied having any involvement in the incident, and the ShadowBlades' special battleship was never made. Skat has failed his mission, and he was so close. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      }
+    ]
+  },
+  {
+    "id": "l1",
+    "file": "l1.gif",
+    "captions": ["Without a second thought, Skat strikes, killing Strix. After years of dedication to the ShadowBlades, Skat has grown tired of serving the clan."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "l2"
+      }
+    ]
+  },
+  {
+    "id": "l2",
+    "file": "l2.gif",
+    "captions": ["Strix was the closest thing to a friend Skat has ever known, and now it's over. A new life lies ahead for Skat. He knows they will send more troops after him, and he must now leave his home in search of a new one."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "l3"
+      }
+    ]
+  },
+  {
+    "id": "l3",
+    "file": "l3.gif",
+    "captions": ["Skat moves to a faraway island inhabited by pigs, where he lives out the majority of his life before he is found by the ShadowBlades and killed. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      }
+    ]
+  },
+  {
+    "id": "m1",
+    "file": "m1.gif",
+    "captions": ["Remembering all the memories he's shared with Strix, Skat could not bring himself to kill his childhood friend. He walks away, leaving Strix broken on the floor."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "m2"
+      }
+    ]
+  },
+  {
+    "id": "m2",
+    "file": "m2.gif",
+    "captions": ["Strix has been the closest thing to a friend Skat has known. He knew Strix would have shown him the same mercy. Friends for life, Brothers forever..."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "m3"
+      }
+    ]
+  },
+  {
+    "id": "m3",
+    "file": "m3.gif",
+    "captions": ["Like a true ninja, Strix has taken Skat's moment of compassion and turned it against him. Strix delivers a powerful blow to Skat's kidney, killing him instantly. Death has taught Skat his final lesson in life. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      },
+    ]
+  },
+  {
+    "id": "n1",
+    "file": "n1.gif",
+    "captions": ["With the last bit of strength in Skat's body, he attempts one last attack..."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "n2"
+      }
+    ]
+  },
+  {
+    "id": "n2",
+    "file": "n2.gif",
+    "captions": ["'He, he, he. You are weaker than I've ever imagined,' Strix raises his hand, preparing to hit Skat with one last blow. 'You disappoint me, Skat. You should have just accepted the mission, but no. You had to insult the clan.'"],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "n3"
+      }
+    ]
+  },
+  {
+    "id": "n3",
+    "file": "n3.gif",
+    "captions": ["'Why?!' Strix kills Skat. The ShadowBlades never figured out why Skat would betray them, but they still needed someone to complete the mission. Strix was given the task. He accepted and completed the mission. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      }
+    ]
+  },
+  {
+    "id": "o1",
+    "file": "o1.gif",
+    "captions": ["Strix usually finishes his opponents off without hesitation, but this was different. Skat has been Strix's friend since childhood."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "o2"
+      }
+    ]
+  },
+  {
+    "id": "o2",
+    "file": "o2.gif",
+    "captions": ["Strix decides to let Skat free with a warning: 'Go, but if we ever meet again, know this: I will kill you.' Surprised, Skat limps away. He knows how the ShadowBlades work. They will hunt him down until he is dead."],
+    "choices": [
+      {
+        "text": "Next",
+        "url": "o3"
+      }
+    ]
+  },
+  {
+    "id": "o3",
+    "file": "o3.gif",
+    "captions": ["Skat spends the rest of his life in hiding. His death came to him in the form of a rare illness. He died knowing no one knew who he really was. END."],
+    "choices": [
+      {
+        "text": "The End",
+        "url": "bad"
+      }
+    ]
+  }
+]
+
+export {
+  meta,
+  data
+}
