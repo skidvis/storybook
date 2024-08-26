ee2b4d7e1d3aae4a63e4a9e707925d44e42cfc35
Skid Vis <skid@skidvis.com>
2024-08-26 14:00:11 -0500

    prettier chanages

--- src/components/vue/Player.vue
+++ src/components/vue/Player.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class=" grid grid-cols-2 md:grid-cols-3 bg-slate-300 shadow-md shadow-slate-700 mb-1">        
-        <div class="font-bold text-center p-1 min-w-36 col-span-2 md:col-span-1">{{meta.title}}</div>        
-        <div class="font-bold text-center p-1 min-w-36" title="Good Endings">Good: <span>{{ goodEndings.length }}</span>/{{meta.goodEndings}}</div>            
+    <div class=" grid grid-cols-2 md:grid-cols-3 bg-slate-300 shadow-md shadow-slate-700 mb-1">
+        <div class="font-bold text-center p-1 min-w-36 col-span-2 md:col-span-1">{{meta.title}}</div>
+        <div class="font-bold text-center p-1 min-w-36" title="Good Endings">Good: <span>{{ goodEndings.length }}</span>/{{meta.goodEndings}}</div>
         <div class="font-bold text-center p-1 min-w-36" title="Bad Endings">Bad: <span>{{ badEndings.length }}</span>/{{meta.badEndings}}</div>
-    </div>  
+    </div>
     <div class="">
         <div :ref="currentPage.id" :key="currentPage.id" class="">
             <div v-if="currentPage != null" class="flex justify-center flex-col md:flex-row p-2">
-                <div>            
-                    <img :src="'/'+ meta.root + '/' + currentPage.file" :key="currentPage.file" alt="" class="m-auto">         
+                <div>
+                    <img :src="'/'+ meta.root + '/' + currentPage.file" :key="currentPage.file" alt="" class="m-auto">
                 </div>
                 <div class="p-0 max-w-md">
                     <ul class="comic-bold">
@@ -20,7 +20,7 @@
                 <li v-for="choice in currentPage.choices" class="flex">
                     <button @click="nextPage(choice.url)" class="border-2 border-lime-800 hover:border-lime-900 p-1 m-auto bg-lime-700 hover:bg-lime-800 font-bold text-white rounded-md min-w-36 shadow-sm shadow-lime-900 sha">{{ choice.text }}</button>
                 </li>
-            </ul>              
+            </ul>
         </div>
     </div>
 </template>
@@ -28,7 +28,7 @@
 <script setup>
     //imports
     import { useGtm } from '@gtm-support/vue-gtm';
-    import {meta, data} from '../../data/skat1/info.js';
+    import { meta, data } from '../../data/skat1/info.js';
     import { ref } from 'vue';
     import { gsap } from 'gsap';
 
@@ -44,29 +44,29 @@
                 url: 'a0'
             }
         ]
-    }    
+    }
     let audioElement = null;
     let goodEndings = [];
     let badEndings = [];
     // countBadEndings();
 
-    if(data) currentPage.value = blank;
+    if (data) currentPage.value = blank;
 
     //methods
-    const nextPage = (url)=>{        
-        if(url === 'winner' && !goodEndings.includes(currentPage.value.id)) {
+    const nextPage = (url) => {
+        if (url === 'winner' && !goodEndings.includes(currentPage.value.id)) {
             goodEndings.push(currentPage.value.id);
         }
-        if(url === 'bad' && !badEndings.includes(currentPage.value.id)) {
+        if (url === 'bad' && !badEndings.includes(currentPage.value.id)) {
             badEndings.push(currentPage.value.id);
         }
-        if(url === 'bad' || url === 'winner') {            
+        if (url === 'bad' || url === 'winner') {
             window.dataLayer.push({
                 event: 'book_end',
                 page_title: meta.title,
-                event_category: 'book_end', 
+                event_category: 'book_end',
                 event_type: url,
-                event_label: currentPage.value.id 
+                event_label: currentPage.value.id
             });
             currentPage.value = blank;
             return;
@@ -76,14 +76,14 @@
         playAudio(nextPageFound.id);
     }
 
-    const playAudio = (clip)=>{
-        if(audioElement != null) audioElement.pause();
-        audioElement = new Audio('/'+ meta.root + '/audio/' + clip + '.mp3');
-        audioElement.load();   
-        audioElement.play();    
+    const playAudio = (clip) => {
+        if (audioElement != null) audioElement.pause();
+        audioElement = new Audio('/' + meta.root + '/audio/' + clip + '.mp3');
+        audioElement.load();
+        audioElement.play();
     }
 
-    const countBadEndings = ()=>{
+    const countBadEndings = () => {
         data.forEach(element => {
             // element.choices.forEach(choice => {
             //     if(choice.url === 'bad') meta.badEndings++;
@@ -94,15 +94,15 @@
     }
 </script>
 
-<style>    
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.1s ease;
-  position: absolute;
-}
+<style>
+    .v-enter-active,
+    .v-leave-active {
+        transition: opacity 0.1s ease;
+        position: absolute;
+    }
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
+    .v-enter-from,
+    .v-leave-to {
+        opacity: 0;
+    }
 </style>
